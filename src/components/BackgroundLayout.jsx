42941ef7620e819439e1254fd97e811739b64e78
Dao Dang Quang <dquang.haiphong@gmail.com>
2023-07-31 03:29:15 +0700

    first commit

---
+++ src/components/BackgroundLayout.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { useStateContext } from "../Context";
+
+//images
+import Clear from "../assets/images/Clear.jpg";
+import Fog from "../assets/icons/fog.png";
+import Cloudy from "../assets/images/Cloudy.jpg";
+import Rainy from "../assets/images/Rainy.jpg";
+import Snow from "../assets/images/Snow.jpg";
+import Stormy from "../assets/images/Stormy.jpg";
+import Sunny from "../assets/images/Sunny.jpg";
+
+const BackgroundLayout = () => {
+  const { weather } = useStateContext();
+  const [image, setImage] = useState(Clear);
+
+  useEffect(() => {
+    if (weather.conditions) {
+      let imageString = weather.conditions;
+      if (imageString.toLowerCase().includes("Clear")) {
+        setImage(Clear);
+      } else if (imageString.toLowerCase().includes("cloud")) {
+        setImage(Cloudy);
+      } else if (
+        imageString.toLowerCase().includes("Rain") ||
+        imageString.toLowerCase().includes("shower")
+      ) {
+        setImage(Rainy);
+      } else if (imageString.toLowerCase().includes("Snow")) {
+        setImage(Snow);
+      } else if (imageString.toLowerCase().includes("Fog")) {
+        setImage(Fog);
+      } else if (
+        imageString.toLowerCase().includes("Thunder") ||
+        imageString.toLowerCase().includes("storm")
+      ) {
+        setImage(Stormy);
+      }
+    }
+  }, [weather]);
+
+  return (
+    <img
+      src={image}
+      alt="weather_image"
+      className="h-screen w-full fixed left-0 top-0 -z-[]"
+    />
+  );
+};
+
+export default BackgroundLayout;
